af9bedbe3947e99beabc1bd4d76ede8d8e864753
zubyCharles <zubecharles@gmail.com>
2022-08-07 05:32:19 +0100

    modified drag and drop

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -52,7 +52,7 @@ const TodoList = () => {
                   )}
                 </Draggable>
               ))}
-              {/* {provided.placeholder} */}
+              {provided.placeholder}
             </ul>
           )}
         </Droppable>
